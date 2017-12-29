1fb3b6ec2db51bcdbee9436fb6c71651f95a3787
Alivelu Ravula <alivelu7193@achiever-25.local>
2017-12-29 14:17:59 +0530

    Initial Commit

---
+++ initWith/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  initWith
+//
+//  Created by Alivelu Ravula on 12/29/17.
+//  Copyright © 2017 Alivelu Ravula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
